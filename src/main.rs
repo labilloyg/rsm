9c4b4e06f8e75bfbabfd3f07f9f62d420b49044f
Guillaume Labiloy <guillaume.labilloy@jax.ufl.edu>
2025-01-25 15:29:23 -0500

    +ADD Cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,9 @@
-// use std::hash;
-
-use std::i32;
-
 use macroquad::prelude::*;
 
-use macroquad::ui::{hash, root_ui, widgets, Skin};
-
-// struct Diagram {
-//     pub total: widgets::Editbox,
-//     pub x: widgets::Editbox,
-//     pub y: widgets::Editbox,
-// }
+use macroquad::ui::{hash, root_ui, widgets};
 
 #[macroquad::main("UI showcase")]
 async fn main() {
-    let _ = {
-        let font = load_ttf_font("../ui_assets/HTOWERT.TTF").await.unwrap();
-        let label_style = root_ui()
-            .style_builder()
-            .with_font(&font)
-            .unwrap()
-            .text_color(Color::from_rgba(180, 180, 120, 255))
-            .font_size(30)
-            .build();
-
-        let window_style = root_ui()
-            .style_builder()
-            .background(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/window_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .background_margin(RectOffset::new(20.0, 20.0, 10.0, 10.0))
-            .margin(RectOffset::new(-20.0, -30.0, 0.0, 0.0))
-            .build();
-
-        let button_style = root_ui()
-            .style_builder()
-            .background(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
-            .margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
-            .background_hovered(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_hovered_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .background_clicked(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_clicked_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .with_font(&font)
-            .unwrap()
-            .text_color(Color::from_rgba(180, 180, 100, 255))
-            .font_size(40)
-            .build();
-
-        // mark1
-        let editbox_style = root_ui()
-            .style_builder()
-            .color(Color::from_rgba(0, 0, 0, 0))
-            .color_clicked(Color::from_rgba(0, 0, 0, 0))
-            .background(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
-            .margin(RectOffset::new(50.0, 50.0, 10.0, 0.0))
-            .background_hovered(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_hovered_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .background_clicked(
-                Image::from_file_with_format(
-                    include_bytes!("../ui_assets/button_clicked_background.png"),
-                    None,
-                )
-                .unwrap(),
-            )
-            .with_font(&font)
-            .unwrap()
-            .text_color(Color::from_rgba(180, 180, 100, 255))
-            .font_size(40)
-            .build();
-
-        Skin {
-            editbox_style,
-            window_style,
-            button_style,
-            label_style,
-            ..root_ui().default_skin()
-        }
-    };
-
     // let default_skin = root_ui().default_skin().clone();
     // let mut window1_skin = skin1.clone();
 
@@ -128,25 +21,20 @@ async fn main() {
             widgets::Group::new(hash!("group"), vec2(screen_width(), screen_height() / 3.0))
                 .position(vec2(0.0, 0.5 * screen_height() / 3.0))
                 .ui(ui, |ui| {
-                    widgets::Label::new("Diagram")
-                        // .position(vec2(200.0, 0.0))
-                        .ui(ui);
+                    widgets::Label::new("Diagram").ui(ui);
                     widgets::InputText::new(hash!())
                         .size(vec2(100.0, 30.0))
                         .filter_numbers()
-                        // .position(vec2(50.0, 70.0))
                         .ui(ui, &mut result);
 
                     widgets::InputText::new(hash!())
                         .size(vec2(100.0, 30.0))
                         .filter_numbers()
-                        // .position(vec2(100.0, 140.0))
                         .ui(ui, &mut x1);
 
                     widgets::InputText::new(hash!())
                         .size(vec2(100.0, 30.0))
                         .filter_numbers()
-                        // .position(vec2(100.0, 140.0))
                         .ui(ui, &mut x2);
 
                     if widgets::Button::new("Answer!").ui(ui) {
