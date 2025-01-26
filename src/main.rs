4d7bdead00781ec6f2fdf3355d5b45b9b7d5cd5e
Guillaume Labiloy <guillaume.labilloy@jax.ufl.edu>
2025-01-26 14:48:46 -0500

    +ADD Result test logic

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,25 @@ struct State {
     target: DiagramValue,
 }
 
+impl State {
+    fn check_result(&self) -> String {
+        let res = self.result.parse::<i32>();
+        let x1_val = self.x1.parse::<i32>();
+        let x2_val = self.x2.parse::<i32>();
+
+        match (res, x1_val, x2_val) {
+            (Ok(res), Ok(x1_val), Ok(x2_val)) => {
+                if res == x1_val + x2_val {
+                    String::from("Corret!")
+                } else {
+                    String::from("Try again :)")
+                }
+            }
+            _ => String::from("Invalid input!"),
+        }
+    }
+}
+
 impl Default for State {
     fn default() -> Self {
         Self {
@@ -68,36 +87,38 @@ async fn main() {
                 .ui(ui, |ui| {
                     if widgets::Button::new(state.result.clone())
                         .size(vec2(100., 100.))
+                        .position(vec2(100., 0.))
                         .ui(ui)
                     {
                         state.target = DiagramValue::Result;
                         state.pop_up_buf = state.result.clone();
                         state.open_window = true;
-                        println!("You pushed the button 1!");
+                        state.dialog = "You pushed the button 1!".to_string();
                     };
 
                     if widgets::Button::new(state.x1.clone())
                         .size(vec2(100., 100.))
-                        .position(vec2(200., 0.))
+                        .position(vec2(0., 100.))
                         .ui(ui)
                     {
                         state.target = DiagramValue::X1;
                         state.pop_up_buf = state.x1.clone();
                         state.open_window = true;
-                        println!("You pushed the button 2!");
+                        state.dialog = "You pushed the button 2!".to_string();
                     };
 
                     if widgets::Button::new(state.x2.clone())
                         .size(vec2(100., 100.))
-                        .position(vec2(100., 100.))
+                        .position(vec2(200., 100.))
                         .ui(ui)
                     {
                         state.target = DiagramValue::X2;
                         state.pop_up_buf = state.x2.clone();
                         state.open_window = true;
-                        println!("You pushed the button 3!");
+                        "You pushed the button 3!".to_string();
                     };
                 });
+            widgets::Editbox::new(hash!(), vec2(200., 30.)).ui(ui, &mut state.dialog);
         });
 
         root_ui().pop_skin();
@@ -111,7 +132,7 @@ async fn main() {
                         .filter_numbers()
                         .ui(ui, &mut state.pop_up_buf);
 
-                    if widgets::Button::new("Quit")
+                    if widgets::Button::new("Go!")
                         .size(vec2(60., 30.))
                         .position(vec2(50., 50.))
                         .ui(ui)
@@ -128,63 +149,12 @@ async fn main() {
                             }
                             DiagramValue::Undefined => {}
                         }
-                        println!("Exiting from reveal!");
                         state.open_window = false;
+                        state.dialog = state.check_result();
                     };
                 },
             );
         };
-
-        // Window definition
-        // widgets::Window::new(hash!(), vec2(0., 0.), vec2(300., 400.)).ui(&mut root_ui(), |ui| {
-        //     widgets::Group::new(hash!(), vec2(screen_width(), screen_height() / 3.0))
-        //         .position(vec2(0.0, 0. * screen_height() / 3.0))
-        //         .ui(ui, |ui| {
-        //             widgets::Label::new("Diagram").ui(ui);
-        //             widgets::InputText::new(hash!())
-        //                 .size(vec2(100.0, 30.0))
-        //                 .filter_numbers()
-        //                 .ui(ui, &mut result);
-        //
-        //             widgets::InputText::new(hash!())
-        //                 .size(vec2(100.0, 30.0))
-        //                 .filter_numbers()
-        //                 .ui(ui, &mut x1);
-        //
-        //             widgets::InputText::new(hash!())
-        //                 .size(vec2(100.0, 30.0))
-        //                 .filter_numbers()
-        //                 .ui(ui, &mut x2);
-        //
-        //             if widgets::Button::new("Answer!").ui(ui) {
-        //                 let res = result.parse::<i32>();
-        //                 let x1_val = x1.parse::<i32>();
-        //                 let x2_val = x2.parse::<i32>();
-        //
-        //                 match (res, x1_val, x2_val) {
-        //                     (Ok(res), Ok(x1_val), Ok(x2_val)) => {
-        //                         dialog = if res == x1_val + x2_val {
-        //                             String::from("Corret!")
-        //                         } else {
-        //                             String::from("Try again :)")
-        //                         };
-        //                     }
-        //                     _ => {
-        //                         dialog = String::from("Invalid input!");
-        //                     }
-        //                 }
-        //             };
-        //
-        //             if widgets::Button::new("Reset!").ui(ui) {
-        //                 result = String::from("12");
-        //                 x1 = String::from("<x1>");
-        //                 x2 = String::from("4");
-        //                 dialog = String::from("Waiting..");
-        //             };
-        //             widgets::Editbox::new(hash!(), vec2(200., 30.)).ui(ui, &mut dialog);
-        //         });
-        // });
-
         root_ui().pop_skin();
 
         next_frame().await;
